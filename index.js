23cd3bca6638650c7703a1966239cb6f7d410d54
Jeremiah Newman <jeremiah.n.a.newman@gmail.com>
2022-01-28 15:41:56 -0500

    added cust123 to collection from vsc

--- index.js
+++ index.js
@@ -21,3 +21,73 @@ initializeApp({
 // connect to Firestore
 const db = getFirestore();
 
+// create a collection called "restaurants" as a variable 'restRef'
+const shopRef = db.collection('customers')
+
+// create a doc
+shopRef.doc('cust123').set({
+    firstName: "Archie",
+    lastName: "Octavius",
+    billingAddress1: "777 Heaven's Door",
+    billingAddress2: "",
+    city: "Hell",
+    state: "MI",
+    postal: "49232", 
+    email: "paradoxparadigm@yahoo.com",
+    phone: "8108746734",
+    returnCust: false,
+})
+ .then(()=> {
+     console.log('Added Customer')
+ }).catch((err)=>{console.error(err)})
+
+
+// read a doc
+
+// read a collection
+
+// update a field and add a field to a doc
+
+
+
+
+
+
+
+// add each restaurant
+// restRef.add(restaurants[3]) // this will go into our database and add a restaurant as a doc with all that information
+//  .then(doc=>{
+//      console.log('Added restaurant', doc.id);
+//  })
+//  .catch(err =>{
+//       console.error(err);
+//  });
+
+// read one document
+// db.collection("restaurants")
+//   .doc("fLhiCGRmKoJ20hm4VPPj")
+//   .get()
+//   .then((doc) => {
+//     console.log(doc.id, "-->", doc.data()); // doc.data gets us the data from the document
+//   })
+//   .catch((err) => console.error(err));
+
+// get all documents
+// restRef
+//   .get()
+//   .then((snapshot) => {
+//     //by convention we call getting a whole collection a snapshot
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, "-->", doc.data());
+//     });
+//   }) // callback function
+//   .catch(console.error()); // this is equivalent to (err) => console.error(err)
+
+// // querying a collection
+// restRef.where('name', '==', 'Bolay').get()
+//  .then(snapshot => {
+//      snapshot.forEach(doc => {
+//          console.log(doc.data())
+//      });
+//  })
+//  .catch(console.error);
